3c6dcf96a4056c410862674a755b1df5a81c090c
vanderlindenma <vanderlindenma@users.noreply.github.com>
2024-11-25 17:32:09 -0500

    Show/Hide prediction Acknowledge Alert button (not hooked)

--- app/dashboard/_components/Dashboard.tsx
+++ app/dashboard/_components/Dashboard.tsx
@@ -11,6 +11,7 @@ import {
   PlayIcon,
   PauseIcon,
   ArrowDownTrayIcon,
+  CheckCircleIcon,
 } from "@heroicons/react/24/solid";
 
 export default function Dashboard(props: { grouped_events: groupedEventList }) {
@@ -30,6 +31,7 @@ export default function Dashboard(props: { grouped_events: groupedEventList }) {
   const [selectedEventId, setSelectedEventId] = useState<number>(data[0].id);
   const [selectedEvent, setSelectedEvent] = useState<groupedEvent>(data[0]);
   const [imageId, setImageId] = useState<number>(0);
+  const [showPrediction, setShowPrediction] = useState<boolean>(true);
 
   const [isPlaying, setIsPlaying] = useState(false);
 
@@ -78,7 +80,7 @@ export default function Dashboard(props: { grouped_events: groupedEventList }) {
               alt={`Event media ${index}`}
               className="w-full h-auto rounded-md"
             />
-            {selectedEvent.localizations[index] && (
+            {showPrediction && selectedEvent.localizations[index] && (
               <div
                 key={`prediction_rectangle_${index}`}
                 className="absolute border-2 border-red-500"
@@ -92,39 +94,83 @@ export default function Dashboard(props: { grouped_events: groupedEventList }) {
             )}
           </div>
         ))}
-        <div className="flex flex-row items-center justify-center">
-          <Button
-            onClick={() => setIsPlaying(!isPlaying)}
-            className="mr-2 w-12"
-          >
-            {isPlaying ? (
-              <PauseIcon className="w-6 h-6" />
-            ) : (
-              <PlayIcon className="w-6 h-6" />
-            )}
-          </Button>
-          <div className="flex-1 items-center relative">
-            <CustomSlider
-              className="w-full cursor-pointer"
-              value={[imageId]}
-              onValueChange={(value: number[]) => setImageId(value[0])}
-              min={0}
-              max={selectedEvent?.media_urls.length - 1}
-              step={1}
-            />
+        <div className="flex flex-row items-center justify-center gap-x-6">
+          <div className="flex-1 flex flex-row items-center justify-center gap-x-2">
+            <Button onClick={() => setIsPlaying(!isPlaying)} className="w-12">
+              {isPlaying ? (
+                <PauseIcon className="w-6 h-6" />
+              ) : (
+                <PlayIcon className="w-6 h-6" />
+              )}
+            </Button>
+            <div className="flex-1 items-center relative">
+              <CustomSlider
+                className="w-full cursor-pointer"
+                value={[imageId]}
+                onValueChange={(value: number[]) => setImageId(value[0])}
+                min={0}
+                max={selectedEvent?.media_urls.length - 1}
+                step={1}
+              />
+            </div>
           </div>
-          <Button
-            onClick={() => {
-              const link = document.createElement("a");
-              link.href = selectedEvent?.media_urls[imageId];
-              link.download = `event_media_${imageId}.jpg`;
-              link.click();
-            }}
-            className="ml-2"
-          >
-            Download Image
-            <ArrowDownTrayIcon className="w-6 h-6" />
-          </Button>
+          <div className="flex flex-row gap-x-2">
+            <Button
+              variant={showPrediction ? "default" : "outline"}
+              onClick={() => setShowPrediction(!showPrediction)}
+            >
+              <span>
+                {showPrediction ? "Hide Prediction" : "Show Prediction"}
+              </span>
+              <span
+                style={{ transform: "scaleY(1.5)" }}
+                className={showPrediction ? "crossed-out" : ""}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M5.25 7.5A2.25 2.25 0 0 1 7.5 5.25h9a2.25 2.25 0 0 1 2.25 2.25v9a2.25 2.25 0 0 1-2.25 2.25h-9a2.25 2.25 0 0 1-2.25-2.25v-9Z"
+                  />
+                </svg>
+              </span>
+            </Button>
+            <Button
+              onClick={() => {
+                const link = document.createElement("a");
+                link.href = selectedEvent?.media_urls[imageId];
+                link.download = `event_media_${imageId}.jpg`;
+                link.click();
+              }}
+            >
+              Download Image
+              <ArrowDownTrayIcon className="w-6 h-6" />
+            </Button>
+            <Button>
+              Acknowledge Alert
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+              </svg>
+            </Button>
+          </div>
         </div>
       </div>
 
