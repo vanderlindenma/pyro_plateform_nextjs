053fec0b32428e450f26a345faa13cf3676a32ee
vanderlindenma <vanderlindenma@users.noreply.github.com>
2025-03-04 15:54:34 +0100

    Image loading screen bug

--- app/dashboard/_components/dashboard_components/AlertDisplay.tsx
+++ app/dashboard/_components/dashboard_components/AlertDisplay.tsx
@@ -1,26 +1,32 @@
 import React from "react";
-import { useContext } from "react";
+import { useContext, useRef, useState, useEffect } from "react";
 import { DashboardContext } from "../Dashboard";
 
 const AlertDisplay = () => {
   const { selectedEvent, imageId, showPrediction } =
     useContext(DashboardContext);
-  const [isLoading, setIsLoading] = React.useState(true);
-  const [showLoadingUI, setShowLoadingUI] = React.useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [showLoadingUI, setShowLoadingUI] = useState(false);
+  const isMounted = useRef<boolean>(false);
 
-  // Reset loading state when URL changes
-  React.useEffect(() => {
-    if (selectedEvent?.media_urls[imageId]) {
-      setIsLoading(true);
-      setShowLoadingUI(false);
-      
-      const loadingTimer = setTimeout(() => {
-        setShowLoadingUI(true);
-      }, 200);
+  // Reset loading state when selectedEvent changes
+  useEffect(() => {
+    if(isMounted.current){
+      console.log("selectedEvent changed");
+      if (selectedEvent?.id) {
+        setIsLoading(true);
+        setShowLoadingUI(false);
+        
+        const loadingTimer = setTimeout(() => {
+          setShowLoadingUI(true);
+        }, 200);
 
-      return () => clearTimeout(loadingTimer);
+        return () => clearTimeout(loadingTimer);
+      }
+    } else {
+      isMounted.current = true;
     }
-  }, [selectedEvent?.media_urls[imageId]]);
+  }, [selectedEvent?.id]);
 
   return (
     <div>
@@ -58,7 +64,7 @@ const AlertImage = ({
   showLoadingUI: boolean;
   onLoad: () => void;
 }) => {
-  const [loadError, setLoadError] = React.useState(false);
+  const [loadError, setLoadError] = useState(false);
 
   return (
     <div className="relative">
@@ -73,7 +79,7 @@ const AlertImage = ({
       <div className={`${loadError && !isLoading ? '' : 'hidden'} flex items-center justify-center bg-gray-100 rounded-md w-full aspect-[1280/720]`}>
         <div className="flex flex-col items-center gap-2">
           <svg width="60px" height="60px" viewBox="15 15 45 45">
-            <path fill="#6B7280" stroke-width="0.2" stroke-linejoin="round" d="M 27.9999,56L 17,56L 17,20L 59,20L 59,28L 49.9999,28.0001L 50,39L 38.9999,39.0001L 39,50L 38.9999,50.0001L 28,50L 27.9999,50.0001L 27.9999,56 Z M 37.9999,38.0001L 42.7499,38.0001L 45.9166,34.8334L 48.9998,37.9166L 48.9998,27.0002L 56,27L 56,23L 20,23L 20,53L 26.9999,53.0001L 26.9999,51L 24,51L 30.0833,44.3333L 33.25,47.5L 37.9999,42.7501L 37.9999,38.0001 Z M 30.9999,56.0001L 30.9999,53.0001L 41.9999,53.0001L 41.9999,42.0001L 42,42L 52.9999,42.0001L 53,42L 53,31L 59,31L 59,56L 30.9999,56.0001 Z M 42.9998,53.0001L 56,53L 56,32L 54,32L 54,51L 42.9998,51L 42.9998,53.0001 Z M 30.0833,26.9167C 30.0833,30.4145 27.2478,33.25 23.75,33.25C 23.2033,33.25 22.5061,33.1303 22,33L 22,25L 29.6666,25C 29.7968,25.5061 30.0833,26.37 30.0833,26.9167 Z "/>
+            <path fill="#6B7280" strokeWidth="0.2" strokeLinejoin="round" d="M 27.9999,56L 17,56L 17,20L 59,20L 59,28L 49.9999,28.0001L 50,39L 38.9999,39.0001L 39,50L 38.9999,50.0001L 28,50L 27.9999,50.0001L 27.9999,56 Z M 37.9999,38.0001L 42.7499,38.0001L 45.9166,34.8334L 48.9998,37.9166L 48.9998,27.0002L 56,27L 56,23L 20,23L 20,53L 26.9999,53.0001L 26.9999,51L 24,51L 30.0833,44.3333L 33.25,47.5L 37.9999,42.7501L 37.9999,38.0001 Z M 30.9999,56.0001L 30.9999,53.0001L 41.9999,53.0001L 41.9999,42.0001L 42,42L 52.9999,42.0001L 53,42L 53,31L 59,31L 59,56L 30.9999,56.0001 Z M 42.9998,53.0001L 56,53L 56,32L 54,32L 54,51L 42.9998,51L 42.9998,53.0001 Z M 30.0833,26.9167C 30.0833,30.4145 27.2478,33.25 23.75,33.25C 23.2033,33.25 22.5061,33.1303 22,33L 22,25L 29.6666,25C 29.7968,25.5061 30.0833,26.37 30.0833,26.9167 Z "/>
           </svg>
           <p className="text-gray-500">Images could not be loaded</p>
         </div>
